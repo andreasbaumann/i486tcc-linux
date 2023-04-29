0e1dd3ef4a26f23976520bbf65b6ee8b57e42552
Andreas Baumann <mail@andreasbaumann.cc>
2023-04-29 11:08:38 +0200

    - more docu on isa on qemu, using more epoch compliant hardware in qemu scripts - testing on real hardware, ne driver is oopsing

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -4,4 +4,4 @@ sleep 2 && \
 qemu-system-i386 -cpu 486 -m 64M \
 	-drive "file=floppy00,if=floppy,format=raw" \
 	-netdev user,id=net0,net=10.0.0.0/24,host=10.0.0.2,dhcpstart=10.0.0.16,hostfwd=tcp::8080-:80,hostfwd=udp::8081-:81 \
-	-device rtl8139,netdev=net0
+	-device ne2k_isa,iobase=0x300,irq=10,netdev=net0
