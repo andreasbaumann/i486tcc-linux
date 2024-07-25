965d66da606b738b3e0cb3e18bedc27a0d0e70ec
Andreas Baumann <mail@andreasbaumann.cc>
2024-07-25 17:32:16 +0200

    - updated Linux kernel to 6.10.1 - updated sbase to b30fb - updates ubase to a570a - updated vis to a7aac - updated meh to a8da4f8 - updated slock to a34d8fb

--- scripts/run_qemu_direct.sh
+++ scripts/run_qemu_direct.sh
@@ -6,6 +6,6 @@ qemu-system-i386 -cpu 486 -m 24M -machine isapc \
  	-kernel bzImage -initrd ramdisk.img \
  	-append	"debug loglevel=7 earlycon=vga earlycon=uart8250,io,0x3f8,9600n8 console=tty0 console=ttyS0,9600n8 iommu=off init=/init vga=0x318" \
 	-netdev user,id=net0,net=10.0.0.0/24,host=10.0.0.2,dhcpstart=10.0.0.16,hostfwd=tcp::2222-:22,hostfwd=tcp::6001-:6000 \
-	-vga std -vnc :0 \
+	-vga std -vnc `hostname`:0 \
 	-device ne2k_isa,iobase=0x300,irq=10,netdev=net0
 pkill qemu-nbd
