ddc9ceb5e98041aa344874aeb5ebbda7136c8c8f
Andreas Baumann <mail@andreasbaumann.cc>
2025-07-17 15:36:32 +0200

    forgot to remove some tools and helper files in clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -15,6 +15,8 @@ rm -rf "${BASE}/root.img"
 rm -rf "${BASE}/root"/*
 rm -rf "${BASE}"/floppy*
 rm -rf "${BASE}/"{data.tar,bzImage,ramdisk.img,EOF}
+rm -rf PAD
+rm -f tools/bdftopcf tools/rdev tools/ucs2any
 
 exit 0
 
