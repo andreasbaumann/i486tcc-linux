16168d00ea478b4981e052941d3770507aed8b82
Andreas Baumann <mail@andreasbaumann.cc>
2025-11-22 14:54:09 +0100

    show OK/UPD for packages in checkversions.sh

--- scripts/checkversions.sh
+++ scripts/checkversions.sh
@@ -22,7 +22,11 @@ for line in `cat "${BASE}/configs/versions"`; do
 	PACKAGE=`echo $line | cut -f 1 -d = | sed -s 's/_VERSION$//'`
 	HAS_VERSION=`echo $line | cut -f 2 -d = | tr -d '"'`
 	CHECK_VERSION=`nvchecker -c "${BASE}/configs/nvchecker.toml" -e "${PACKAGE}" --logger json | jq -r 'select(.event=="updated") | .version'`
-	echo "${PACKAGE} ${HAS_VERSION} ${CHECK_VERSION}"
+	STATE="UPD"
+	if test "x${CHECK_VERSION}" = "x${HAS_VERSION}"; then
+		STATE="OK "
+	fi
+	echo "${STATE} ${PACKAGE} ${HAS_VERSION} ${CHECK_VERSION}"
 done
 
 trap - 0
