bf4735f75c8459a9ce112acb7e6fd0ec52925ffc
Andreas Baumann <mail@andreasbaumann.cc>
2025-11-22 15:19:42 +0100

    run the standard qemu not in vnd mode, otherwise we cannot easily swap floppies

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -5,6 +5,6 @@ sleep 2 && \
 qemu-system-i386 -cpu 486 -m 24M -machine isapc \
 	-drive "file=floppy00.img,if=floppy,format=raw" \
 	-netdev user,id=net0,net=10.0.0.0/24,host=10.0.0.2,dhcpstart=10.0.0.16,hostfwd=tcp::2222-:22,hostfwd=tcp::6001-:6000 \
-	-vga std -vnc :0 \
+	-vga std \
 	-device ne2k_isa,iobase=0x300,irq=10,netdev=net0
 pkill qemu-nbd
