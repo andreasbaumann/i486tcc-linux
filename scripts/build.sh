2c4313b96328314df2989d16100897f1a9148eeb
Andreas Baumann <mail@andreasbaumann.cc>
2026-01-30 12:05:33 +0100

    some preliminary testing of Xvesa (we get a black screen)

--- scripts/build.sh
+++ scripts/build.sh
@@ -551,8 +551,9 @@ if [ "x${CONFIG_TINYXSERVER}" = "xy" ]; then
 		tar xf "${BASE}/downloads/tinyxserver-${TINYXSERVER_VERSION}.tar.gz"
 		cd "tinyxserver-${TINYXSERVER_VERSION}" || exit 1
 		patch -Np1 < "${BASE}/patches/tinyxserver-tcc.patch"	
-		patch -Np1 < "${BASE}/patches/tinyxserver-fbdev.patch"	
-		make -j$CPUS BASE="${BASE}" core Xfbdev xinit
+		patch -Np1 < "${BASE}/patches/tinyxserver-fbdev.patch"
+		patch -Np1 < "${BASE}/patches/tinyxserver-xvesa-vm86.patch"
+		make -j$CPUS BASE="${BASE}" core Xfbdev Xvesa xinit
 		make -j$CPUS BASE="${BASE}" DESTDIR="${BASE}/build/stage1" PREDIR=/ -j$CPUS install
 		cd .. || exit 1
 	else
--- scripts/run_qemu_direct.sh
+++ scripts/run_qemu_direct.sh
@@ -4,8 +4,8 @@ qemu-nbd -f raw root.img -x ROOT &
 sleep 2 && \
 qemu-system-i386 -cpu 486 -m 24M -machine isapc \
  	-kernel bzImage -initrd ramdisk.img \
- 	-append	"debug loglevel=7 earlycon=vga earlycon=uart8250,io,0x3f8,9600n8 console=tty0 console=ttyS0,9600n8 iommu=off init=/init vga=0x318" \
+ 	-append	"debug loglevel=7 earlycon=vga earlycon=uart8250,io,0x3f8,9600n8 console=tty0 console=ttyS0,9600n8 iommu=off init=/init video:vesafb=off" \
 	-netdev user,id=net0,net=10.0.0.0/24,host=10.0.0.2,dhcpstart=10.0.0.16,hostfwd=tcp::2222-:22,hostfwd=tcp::6001-:6000 \
-	-vga std -vnc `hostname`:0 \
+	-vga cirrus -vnc `hostname`:0 \
 	-device ne2k_isa,iobase=0x300,irq=10,netdev=net0
 pkill qemu-nbd
