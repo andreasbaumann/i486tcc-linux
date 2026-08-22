1dbf62603990df1771993e0a904a119ac5c6c6e6
Andreas Baumann <mail@andreasbaumann.cc>
2026-08-22 15:05:23 +0200

    checkversions.sh: better version checking, having an explicit ERR state now

--- scripts/checkversions.sh
+++ scripts/checkversions.sh
@@ -21,9 +21,13 @@ BASE=`dirname ${SCRIPT}`/..
 for line in `cat "${BASE}/configs/versions"`; do
 	PACKAGE=`echo $line | cut -f 1 -d = | sed -s 's/_VERSION$//'`
 	HAS_VERSION=`echo $line | cut -f 2 -d = | tr -d '"'`
-	CHECK_VERSION=`nvchecker -c "${BASE}/configs/nvchecker.toml" -e "${PACKAGE}" --logger json | jq -r 'select(.event=="updated") | .version'`
+	NVOUT=`nvchecker -c "${BASE}/configs/nvchecker.toml" -e "${PACKAGE}" --logger json`
+	CHECK_VERSION=`echo "${NVOUT}" | jq -r 'select(.event=="updated") | .version' 2>/dev/null`
+	JQ_STATUS=$?
 	STATE="UPD"
-	if test "x${CHECK_VERSION}" = "x${HAS_VERSION}"; then
+	if test ${JQ_STATUS} -ne 0 -o "x${CHECK_VERSION}" = "x"; then
+		STATE="ERR"
+	elif test "x${CHECK_VERSION}" = "x${HAS_VERSION}"; then
 		STATE="OK "
 	fi
 	echo "${STATE} ${PACKAGE} ${HAS_VERSION} ${CHECK_VERSION}"
